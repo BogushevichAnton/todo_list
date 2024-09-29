ba745462ac8c704369c4bf4a96985cf0ecf2d650
nemooo-trash <bam2000000@gmail.com>
2024-09-29 21:20:36 +0300

    ediit

--- todo_drf/todo_list/views.py
+++ todo_drf/todo_list/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'todo_list/index.html')
+    '''Коннект к бд'''
+    ''' Тут что то меняем в данных '''
+    ''' бд -> данные парсим в dict '''
+    data = {'словарь1': 123}
+    return render(request, 'todo_list/index.html',
+                  {'data': data, }
+                  )
