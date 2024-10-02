dc00303b335bad4e81e6a779381b0b4160ea9bbc
nemooo-trash <bam2000000@gmail.com>
2024-10-02 23:48:01 +0300

    add normal page

--- todo_drf/todo_list/static/todo_list/js/topics.js
+++ todo_drf/todo_list/static/todo_list/js/topics.js
@@ -1,9 +1,4 @@
-document.getElementById('toggle-menu').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-
-    // Переключаем класс для сворачивания/разворачивания меню
-    sidebar.classList.toggle('collapsed');
-
-    // Изменяем текст кнопки в зависимости от состояния
-    this.textContent = sidebar.classList.contains('collapsed') ? '▶️' : '◀️';
+const toggler = document.querySelector(".toggler-btn");
+toggler.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("collapsed");
 });
