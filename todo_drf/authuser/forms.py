800722c4be816ec14bb94cc13986c20dee1fd2aa
nemooo-trash <bam2000000@gmail.com>
2024-10-03 06:00:12 +0300

    eddit so large commit...

--- todo_drf/authuser/forms.py
+++ todo_drf/authuser/forms.py
@@ -1,4 +1,3 @@
-
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
@@ -6,22 +5,29 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class AuthForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(
-        attrs={'class': 'form-input'}
+        attrs={'class': 'form-control',
+               'type': "email",
+               'placeholder': "Введите email",
+               }
     ))
 
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
-        attrs={'class': 'form-input'}
+        attrs={'type': "password",
+               'class': "form-control",
+               'placeholder': "Введите пароль",
+
+               }
     ))
 
     class Meta:
         model = get_user_model()
+
 
 class RegisterForm(UserCreationForm):
     name = forms.CharField(label='Ваше имя', widget=forms.TextInput(
         attrs={'class': 'form-input'}
     ))
+
     class Meta:
         model = get_user_model()
         fields = ["email", "name", "password1", "password2"]
-
-
