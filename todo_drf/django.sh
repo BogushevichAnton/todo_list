98b1ab3089340b42c3649dff32635afbe8e39e41
nemooo-trash <bam2000000@gmail.com>
2024-12-15 16:50:34 +0300

    end

--- todo_drf/django.sh
+++ todo_drf/django.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Creating Migrations users..."
 python manage.py makemigrations authuser
-echo ====================================
+echo "==================================="
 
 echo "Creating Migrations todo_list..."
 python manage.py makemigrations todo_list
-echo ====================================
+echo "==================================="
 
 echo "Starting Migrations..."
 python manage.py migrate
-echo ====================================
+echo "==================================="
 
 echo "Starting Server..."
 python manage.py runserver 0.0.0.0:8000
